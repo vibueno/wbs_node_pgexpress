caf94a9fc402dac40c7e5ea55438a200e17c6d80
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-02 14:46:27 +0100

    Add Order controller functionality
    -> getAll
    -> getOderById

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,27 +1,22 @@
 // pg config
-const pool = require("../db.js");
-
-const runQuery = async (req, res, query) => {
-  try {
-    const data = await pool.query(query);
-    res.json({
-      code: 200,
-      operation: "success",
-      description: `Fetch all users`,
-      data: data.rows,
-    });
-  } catch (err) {
-    console.error("Error executing query", err);
-  }
-};
+const runQuery = require("../db.js");
 
 const ordersController = {
   getAll: (req, res) => {
-    res.end("returning all orders");
+    const query = {
+      text: "SELECT * FROM orders",
+    };
+
+    runQuery(req, res, query, "Fetch all orders");
   },
 
   getOrderById: (req, res) => {
-    res.end(`returning order with id ${req.params.orderId}`);
+    const query = {
+      text: "SELECT * FROM orders where id=$1",
+      values: [req.params.orderId],
+    };
+
+    runQuery(req, res, query, `Fetch order with id: ${req.params.orderId}`);
   },
 };
 
