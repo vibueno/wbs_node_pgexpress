ef7c45e8d38f475adb62434b508c8b921a45d503
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-02 13:56:00 +0100

    Add DB connection file

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+const { DBUSER, DBHOST, DBNAME, DBPASS, DBPORT } = process.env;
+
+const pool = new Pool({
+  user: DBUSER,
+  host: DBHOST,
+  database: DBNAME,
+  password: DBPASS,
+  port: DBPORT,
+});
+
+module.exports = pool;
