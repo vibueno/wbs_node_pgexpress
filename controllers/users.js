d9e55136134c40c67abeb42322a420735cf674bb
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-02 14:32:36 +0100

    Add User contoller -> getUserById

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,36 @@
 // pg config
 const pool = require("../db.js");
 
+const runQuery = async (req, res, query) => {
+  try {
+    const data = await pool.query(query);
+    res.json({
+      code: 200,
+      operation: "success",
+      description: `Fetch all users`,
+      data: data.rows,
+    });
+  } catch (err) {
+    console.error("Error executing query", err);
+  }
+};
+
 const usersController = {
-  getAll: async (req, res) => {
+  getAll: (req, res) => {
     const query = {
       text: "SELECT * FROM users",
     };
 
-    try {
-      const data = await pool.query(query);
-      res.json({
-        code: 200,
-        operation: "success",
-        description: `Fetch all users`,
-        data: data.rows,
-      });
-    } catch (err) {
-      console.error("Error executing query", err);
-    }
+    runQuery(req, res, query);
   },
 
   getUserById: (req, res) => {
-    res.end(`Returning user with id ${req.params.userId}`);
+    const query = {
+      text: "SELECT * FROM users where id=$1",
+      values: [req.params.userId],
+    };
+
+    runQuery(req, res, query);
   },
 };
 
