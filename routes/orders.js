64136092290a90b3a3267fc0173b07c7f23aa50f
vibueno <20514260+vibueno@users.noreply.github.com>
2021-02-02 15:47:55 +0100

    Move export instructions to end of modules

--- routes/orders.js
+++ routes/orders.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ordersController = require("../controllers/orders");
 
-module.exports = router;
-
 // '/orders/': returns all orders
 router.get("/", ordersController.getAll);
 
 // '/orders/:orderId': returns order with id orderId
 router.get("/:orderId", ordersController.getOrderById);
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
 
-module.exports = router;
-
 // '/users/': returns all users
 router.get("/", usersController.getAll);
 
 // '/users/:userId': returns user with id userId
 router.get("/:userId", usersController.getUserById);
+
+module.exports = router;
